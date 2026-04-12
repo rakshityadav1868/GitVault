67d458c8a61f3fcd5a82a1d875efe0c8fa007d22
Rakshit Yadav <yadavrakshit60@gmail.com>
2026-04-12 21:30:39 +0530

    adding light/dark mode and applying sorting featurew
    Signed-off-by: Rakshit Yadav <yadavrakshit60@gmail.com>

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
+function renderdata(data) {
+    const resultdiv = document.querySelector('.result-section')
+    resultdiv.innerHTML = ""
+    data.forEach(x => {
+        resultdiv.innerHTML +=
+            `
+<div>
+<h2>${x.name}</h2>
+<p>${x.description}</p>
+</div>
+`
+    })
+
+}
+let resdata = []
 document.getElementById("search-btn").addEventListener('click', async () => {
     const username = document.getElementById('search-input').value
     if (!username) {
         alert("Enter Username")
     }
     try {
+
         const res = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
         const data = await res.json()
-        const resultdiv = document.querySelector('.result-section')
-        resultdiv.innerHTML = ""
-
-        data.forEach(x => {
-            resultdiv.innerHTML +=
-                `
-        <div>
-        <h2>${x.name}</h2>
-        <p>${x.description}</p>
-        </div>
-        `
-        })
-
+        resdata = data
+        renderdata(resdata)
     } catch (err) {
         alert("Error:" + err)
     }
+})
+
+const togglebtn = document.getElementById("toggle")
+togglebtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    const isdark = document.body.classList.contains("dark")
+
+    togglebtn.innerText = isdark ? "Light Mode" : "Dark Mode"
+})
+document.getElementById("sort-filter").addEventListener("change", (e) => {
+    let sorted
+
+    if (e.target.value == "a-z") {
+        sorted = [...resdata].sort((a, b) => a.name.localeCompare(b.name))
+    } else if (e.target.value == "z-a") {
+        sorted = [...resdata].sort((a, b) => b.name.localeCompare(a.name))
+
+    } else {
+        sorted = resdata
+    }
+    renderdata(sorted)
 })
